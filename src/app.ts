2c0ccd5a97230a7c3a3966a16ee2f1564d49405a
Chandni <chandni1141996@gmail.com>
2024-02-09 17:51:14 +0530

    Added open layers packages and updated rendering code

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
-declare var ol: any; //global variable
+import Map from "ol/Map.js";
+import OSM from "ol/source/OSM.js";
+import TileLayer from "ol/layer/Tile.js";
+import View from "ol/View.js";
+
+declare var ol: any;
 
 const form = document.querySelector("form")!;
 // const addressInput = document.getElementById("address")! as HTMLInputElement;
@@ -6,21 +11,40 @@ const form = document.querySelector("form")!;
 function searchAddressHandler(event: Event) {
   event.preventDefault();
 
-  const coordinates = { lat: 40.41, lng: -73.99 }; // Can't fetch coordinates from Google API, use dummy ones
+  //   const coordinates = { lat: 40.41, lng: -73.99 }; // Can't fetch coordinates from Google API, use dummy ones
 
-  document.getElementById("map")!.innerHTML = ""; // clear <p> from <div id="map">
-  new ol.Map({
-    target: "map",
+  const map = new Map({
     layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
+      new TileLayer({
+        source: new OSM(),
       }),
     ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
-      zoom: 16,
+    target: "map",
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
     }),
   });
+
+  document.getElementById("map")!.onclick = function () {
+    const view = map.getView();
+    const zoom = view.getZoom()!;
+    view.setZoom(zoom - 1);
+  };
+
+  document.getElementById("map")!.innerHTML = ""; // clear <p> from <div id="map">
+  //   new ol.Map({
+  //     target: "map",
+  //     layers: [
+  //       new ol.layer.Tile({
+  //         source: new ol.source.OSM(),
+  //       }),
+  //     ],
+  //     view: new ol.View({
+  //       center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+  //       zoom: 16,
+  //     }),
+  //   });
 }
 
 form.addEventListener("submit", searchAddressHandler);

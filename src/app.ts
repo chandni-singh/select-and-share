f756d12071f00133e878825db6ebfd803a10efc2
Chandni <chandni1141996@gmail.com>
2024-02-09 17:39:36 +0530

    added map rendering logic

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
+declare var ol: any; //global variable
+
 const form = document.querySelector("form")!;
-const addressInput = document.getElementById("address")! as HTMLInputElement;
+// const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
-  const enteredAddress = addressInput.value;
+
+  const coordinates = { lat: 40.41, lng: -73.99 }; // Can't fetch coordinates from Google API, use dummy ones
+
+  document.getElementById("map")!.innerHTML = ""; // clear <p> from <div id="map">
+  new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16,
+    }),
+  });
 }
 
 form.addEventListener("submit", searchAddressHandler);
